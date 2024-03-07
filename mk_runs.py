91f09a2005d1dc71c3910b18343fb2cfeae9f5d8
Peter Teuben <teuben@gmail.com>
2024-03-07 08:27:28 -0500

    new

--- mk_runs.py
+++ mk_runs.py
@@ -6,20 +6,21 @@ import sys
 
 from lmtoy import runs
 
-project="2023-S1-US-00"
+project="2024-S1-SP-5"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
-on['foo'] = [ ]
+on["NGC_4038S"] = [ 112223, 112225, 112227, 112231, 112251, 112253, 112255,
+                    112261, 112263, 112265,]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1['foo']   = ""
+pars1["NGC_4038S"] = ""
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
-pars2['foo']   = "srdp=1 admit=0"
+pars2["NGC_4038S"] = ""
 
 if __name__ == '__main__':    
     runs.mk_runs(project, on, pars1, pars2, sys.argv)
